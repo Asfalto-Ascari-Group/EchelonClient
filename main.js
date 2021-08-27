7b5be0ac4181e90f9ea838e1a001fa1b6b255cc0
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2021-08-27 08:28:09 +0100

    autoUpdater init

--- main.js
+++ main.js
@@ -18,6 +18,25 @@ setInterval(() => {
     autoUpdater.checkForUpdates();
 }, 10000);
 
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    };
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+});
+
+autoUpdater.on('error', msg => {
+    console.error('There was a problem updating the application')
+    console.error(msg)
+});
+
 // Define variable connection
 const socket = io(`http://34.142.46.24:4644`, {
     reconnection: true,
