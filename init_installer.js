d6d8ad7090704fadc54dc67730917601b264f83a
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2021-12-20 10:15:55 +0000

    dev

--- init_installer.js
+++ init_installer.js
@@ -1,16 +1,18 @@
 const electronInstaller = require('electron-winstaller');
 
-    // NB: Use this syntax within an async function, Node does not have support for
-    //     top-level await as of Node 12.
-    try {
-        electronInstaller.createWindowsInstaller({
+// NB: Use this syntax within an async function, Node does not have support for
+//     top-level await as of Node 12.
+try {
+    electronInstaller.createWindowsInstaller({
         appDirectory: 'E:/SoftwareDev/AA/src4/EchelonClient-Release-Stable/echelon-win32-x64',
         outputDirectory: 'E:/SoftwareDev/AA/src4/EchelonClient-Release-Stable/installer',
-        description: 'description',
-        authors: 'author1',
+        title: 'Echelon',
+        name: 'Echelon',
+        description: 'Project Echelon Executable File',
+        authors: 'AsfaltoAscari',
         exe: 'echelon.exe'
-        });
-        console.log('It worked!');
-    } catch (e) {
-        console.log(`No dice: ${e.message}`);
-    };
+    });
+    console.log('It worked!');
+} catch (e) {
+    console.log(`No dice: ${e.message}`);
+};
